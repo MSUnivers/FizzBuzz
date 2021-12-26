5b5968013dd7df73cb662db882c07e590901d85f
Maroua Sfaxi <sfaxi.maroua@gmail.com>
2021-12-26 22:03:21 +0100

    DRY approach

--- test.java
+++ test.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("please enter a number: ");
-        int number =scanner.nextInt();
-        if (number%3==0 && number%5==0){
-            System.out.println("Fizzbuzz");
-        } else if(number%5==0){
+        int number = scanner.nextInt();
+        if (number % 3 == 0) {
+            if (number % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else {
+                System.out.println("Fizz");
+            }
+        } else if (number % 5 == 0) {
             System.out.println("Buzz");
-        }else if(number%3==0){
-            System.out.println("Fizz");
-        }else
-        System.out.println(number);
+
+        } else System.out.println(number);
     }
 }
